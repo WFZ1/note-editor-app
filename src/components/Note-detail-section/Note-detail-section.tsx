d403ea211ddc1a467c0841d21fe87dc042b69db6
WFZ1 <pavelskumenko@gmail.com>
2021-09-18 17:08:33 +0300

    feat: add fields validation

--- src/components/Note-detail-section/Note-detail-section.tsx
+++ src/components/Note-detail-section/Note-detail-section.tsx
@@ -1,20 +1,40 @@
-import { ChangeEvent, FC } from 'react';
+import { ChangeEvent, FC, useEffect, useState } from 'react';
 import './Note-detail-section.scss';
 import trashCan from '../../assets/icons/trash-can.svg';
 import INoteDetailSectionProps from '../../types/note-detail-section-props.type';
+import INoteProps from '../../types/note-props.type';
 
 export const NoteDetailSection: FC<INoteDetailSectionProps> = (props: INoteDetailSectionProps) => {
   const { note, onChangeNote, onSaveNote, onDeleteNote } = props;
+  const [errors, setErrors] = useState<string[]>([]);
+
+  useEffect(() => {
+    ['title', 'description'].forEach((field) => {
+      if (!note) return;
+
+      const value = note[field as keyof INoteProps] as string;
+      let regex = /^.{3,120}$/;
+
+      if (field === 'description') {
+        regex = /^[\s\S]{5,500}$/;
+      }
+
+      if (!regex.test(value)) {
+        if (!errors.includes(field)) {
+          setErrors([...errors, field]);
+        }
+      } else if (errors.includes(field)) {
+        const newErrors = errors.filter((error) => error !== field);
+        setErrors(newErrors);
+      }
+    });
+  }, [note, errors]);
 
   const changeNote = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     if (!note) return;
 
     const { value, name } = e.target;
-
-    onChangeNote({
-      ...note,
-      [name]: value
-    });
+    onChangeNote({ ...note, [name]: value });
   };
 
   return (
@@ -27,7 +47,7 @@ export const NoteDetailSection: FC<INoteDetailSectionProps> = (props: INoteDetai
         <img src={ trashCan } alt="Trash can" />
       </button>
 
-      <label className="Note-detail-section__box Note-detail-section__box_size_mid">
+      <label className={ `Note-detail-section__box Note-detail-section__box_size_mid ${ errors.includes('title') ? 'Note-detail-section__box_invalid' : '' }` }>
         <span className="Note-detail-section__title">Title</span>
         <input
           className="Note-detail-section__field"
@@ -37,20 +57,20 @@ export const NoteDetailSection: FC<INoteDetailSectionProps> = (props: INoteDetai
           type="text"/>
       </label>
 
-      <label className="Note-detail-section__box">
+      <label className={ `Note-detail-section__box ${ errors.includes('description') ? 'Note-detail-section__box_invalid' : '' }` }>
         <span className="Note-detail-section__title">Description</span>
         <textarea
           className="Note-detail-section__field"
           value={ note.description }
           onChange={ (e) => changeNote(e) }
           name="description"
-          rows={ 10 }
-        />
+          rows={ 10 }/>
       </label>
 
       <button
         className="Note-detail-section__save"
         onClick={ () => onSaveNote() }
+        disabled={ errors.length > 0 }
       >Save</button>
     </div>
   );
